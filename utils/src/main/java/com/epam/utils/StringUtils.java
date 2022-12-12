6d7690b61836f2f013c0a3372d78657350e75681
sergeikonash <38048905+sergeikonash@users.noreply.github.com>
2022-12-12 17:14:33 +0100

    Update StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,12 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        int i = 1;
+        try {
+            i = Integer.parseInt(str.trim());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return i % 2 == 0;
     }
 }
